fc59230a46b608394b40ab8f9363d14a4b96746a
Jason Ekstrand <jason.ekstrand@intel.com>
2018-04-02 15:59:39 -0700

    nir: Remove deref chain support from lower_drawpixels
    Acked-by: Rob Clark <robdclark@gmail.com>
    Acked-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Acked-by: Dave Airlie <airlied@redhat.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/compiler/nir/nir_lower_drawpixels.c
+++ src/compiler/nir/nir_lower_drawpixels.c
@@ -225,19 +225,6 @@ lower_drawpixels_block(lower_drawpixels_state *state, nir_block *block)
                assert(deref->deref_type == nir_deref_type_var);
                lower_texcoord(state, intr);
             }
-         } else if (intr->intrinsic == nir_intrinsic_load_var) {
-            nir_deref_var *dvar = intr->variables[0];
-            nir_variable *var = dvar->var;
-
-            if (var->data.location == VARYING_SLOT_COL0) {
-               /* gl_Color should not have array/struct derefs: */
-               assert(dvar->deref.child == NULL);
-               lower_color(state, intr);
-            } else if (var->data.location == VARYING_SLOT_TEX0) {
-               /* gl_TexCoord should not have array/struct derefs: */
-               assert(dvar->deref.child == NULL);
-               lower_texcoord(state, intr);
-            }
          }
       }
    }
@@ -265,6 +252,11 @@ nir_lower_drawpixels(nir_shader *shader,
       .options = options,
       .shader = shader,
    };
+
+   /* note that this pass already assumes texture/sampler derefs are already
+    * lowered to index
+    */
+   nir_assert_unlowered_derefs(shader, nir_lower_load_store_derefs);
 
    assert(shader->info.stage == MESA_SHADER_FRAGMENT);
 
