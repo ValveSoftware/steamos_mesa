ff7daadca1b335002d9bc198e38a3faa88b34478
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2018-04-18 14:34:54 +0200

    radv: enable/disable predication for the DCC decompression pass
    Performing a DCC decompression pass is currently pretty rare,
    but using predication allows the GPU to skip unnecessary passes.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Dave Airlie <airlied@redhat.com>

--- src/amd/vulkan/radv_meta_fast_clear.c
+++ src/amd/vulkan/radv_meta_fast_clear.c
@@ -602,7 +602,7 @@ radv_emit_color_decompress(struct radv_cmd_buffer *cmd_buffer,
                pipeline = cmd_buffer->device->meta_state.fast_clear_flush.cmask_eliminate_pipeline;
 	}
 
-	if (!decompress_dcc && radv_image_has_dcc(image)) {
+	if (radv_image_has_dcc(image)) {
 		radv_emit_set_predication_state_from_image(cmd_buffer, image, true);
 		cmd_buffer->state.predicating = true;
 	}
@@ -668,7 +668,7 @@ radv_emit_color_decompress(struct radv_cmd_buffer *cmd_buffer,
 					&cmd_buffer->pool->alloc);
 
 	}
-	if (!decompress_dcc && radv_image_has_dcc(image)) {
+	if (radv_image_has_dcc(image)) {
 		cmd_buffer->state.predicating = false;
 		radv_emit_set_predication_state_from_image(cmd_buffer, image, false);
 	}
