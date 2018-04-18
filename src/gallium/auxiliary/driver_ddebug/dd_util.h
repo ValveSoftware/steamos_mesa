2877b6555c51e896afbc9b8b46ba62a076ccbbc6
Dylan Baker <dylan@pnwbakers.com>
2018-04-18 10:21:14 -0700

    gallium: fix ddebug on windows
    By including the proper headers for getpid and for mkdir.
    
    Fixes: 6ff0c6f4ebcb87ea6c6fe5a4ba90b548f666067d
           ("gallium: move ddebug, noop, rbug, trace to auxiliary to improve build times")
    Signed-off-by: Dylan Baker <dylan.c.baker@intel.com>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>

--- src/gallium/auxiliary/driver_ddebug/dd_util.h
+++ src/gallium/auxiliary/driver_ddebug/dd_util.h
@@ -37,9 +37,13 @@
 #include "util/u_debug.h"
 
 #include "pipe/p_config.h"
-#ifdef PIPE_OS_UNIX
+#if defined(PIPE_OS_UNIX)
 #include <unistd.h>
 #include <sys/stat.h>
+#elif defined(PIPE_OS_WINDOWS)
+#include <direct.h>
+#include <process.h>
+#define mkdir(dir, mode) _mkdir(dir)
 #endif
 
 
