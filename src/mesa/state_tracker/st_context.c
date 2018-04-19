450f00e39d40df2d1e82dae9de4c1679b26e0f9d
Jordan Justen <jordan.l.justen@intel.com>
2018-04-19 16:20:53 -0700

    st/mesa: Use ShaderCacheSerializeDriverBlob driver function
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
    Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

--- src/mesa/state_tracker/st_context.c
+++ src/mesa/state_tracker/st_context.c
@@ -759,10 +759,12 @@ st_init_driver_functions(struct pipe_screen *screen,
       screen->get_shader_param(screen, PIPE_SHADER_VERTEX,
                                PIPE_SHADER_CAP_PREFERRED_IR);
    if (preferred_ir == PIPE_SHADER_IR_NIR) {
+      functions->ShaderCacheSerializeDriverBlob =  st_serialise_nir_program;
       functions->ProgramBinarySerializeDriverBlob = st_serialise_nir_program;
       functions->ProgramBinaryDeserializeDriverBlob =
          st_deserialise_nir_program;
    } else {
+      functions->ShaderCacheSerializeDriverBlob =  st_serialise_tgsi_program;
       functions->ProgramBinarySerializeDriverBlob = st_serialise_tgsi_program;
       functions->ProgramBinaryDeserializeDriverBlob =
          st_deserialise_tgsi_program;
