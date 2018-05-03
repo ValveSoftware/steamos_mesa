7cf284f18e6774c810ed6db17b98e597bf96f8a5
Mark Janes <mark.a.janes@intel.com>
2018-05-03 14:28:22 -0700

    Revert "anv/device: expose shaderInt16 support in gen8+"
    This reverts commit 0ba0ac815e078185c1f408ec7078fd1efac1a634.
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=106393
    Reviewed-by: Scott D Phillips <scott.d.phillips@intel.com>

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -759,7 +759,7 @@ void anv_GetPhysicalDeviceFeatures(
                                                   pdevice->info.has_64bit_types,
       .shaderInt64                              = pdevice->info.gen >= 8 &&
                                                   pdevice->info.has_64bit_types,
-      .shaderInt16                              = pdevice->info.gen >= 8,
+      .shaderInt16                              = false,
       .shaderResourceMinLod                     = false,
       .variableMultisampleRate                  = true,
       .inheritedQueries                         = true,
