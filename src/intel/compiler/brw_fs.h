c2297bdf1927bdc1ef77730813930f96be9010d3
Jose Maria Casanova Crespo <jmcasanova@igalia.com>
2018-06-09 11:45:54 +0200

    intel/fs: Remove old 16-bit shuffle/unshuffle functions
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/intel/compiler/brw_fs.h
+++ src/intel/compiler/brw_fs.h
@@ -508,17 +508,6 @@ fs_reg shuffle_64bit_data_for_32bit_write(const brw::fs_builder &bld,
                                           const fs_reg &src,
                                           uint32_t components);
 
-void shuffle_32bit_load_result_to_16bit_data(const brw::fs_builder &bld,
-                                             const fs_reg &dst,
-                                             const fs_reg &src,
-                                             uint32_t first_component,
-                                             uint32_t components);
-
-void shuffle_16bit_data_for_32bit_write(const brw::fs_builder &bld,
-                                        const fs_reg &dst,
-                                        const fs_reg &src,
-                                        uint32_t components);
-
 void shuffle_from_32bit_read(const brw::fs_builder &bld,
                              const fs_reg &dst,
                              const fs_reg &src,
