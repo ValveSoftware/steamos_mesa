fa9e6c235dad578fe2b62cfcf5fc08c475996a94
Tapani Pälli <tapani.palli@intel.com>
2018-06-14 13:06:33 +0300

    i965: enable EXT_render_snorm
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
    Reviewed-by: Nanley Chery <nanley.g.chery@intel.com>

--- src/mesa/drivers/dri/i965/intel_extensions.c
+++ src/mesa/drivers/dri/i965/intel_extensions.c
@@ -103,6 +103,7 @@ intelInitExtensions(struct gl_context *ctx)
    ctx->Extensions.EXT_pixel_buffer_object = true;
    ctx->Extensions.EXT_point_parameters = true;
    ctx->Extensions.EXT_provoking_vertex = true;
+   ctx->Extensions.EXT_render_snorm = true;
    ctx->Extensions.EXT_stencil_two_side = true;
    ctx->Extensions.EXT_texture_array = true;
    ctx->Extensions.EXT_texture_env_dot3 = true;
