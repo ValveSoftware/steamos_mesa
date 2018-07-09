6c746e8fea1507ee3944bd926bfeef049886bd0c
Jason Ekstrand <jason.ekstrand@intel.com>
2018-07-08 21:40:14 -0700

    anv: Bump the advertised patch version to 80
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

--- src/intel/vulkan/anv_extensions.py
+++ src/intel/vulkan/anv_extensions.py
@@ -48,7 +48,7 @@ class ApiVersion:
         self.version = version
         self.enable = _bool_to_c_expr(enable)
 
-API_PATCH_VERSION = 76
+API_PATCH_VERSION = 80
 
 # Supported API versions.  Each one is the maximum patch version for the given
 # version.  Version come in increasing order and each version is available if
