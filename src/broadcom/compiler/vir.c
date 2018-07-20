421e99d7774ef5b8f6fbb272b69b3412c9569180
Eric Anholt <eric@anholt.net>
2018-07-20 13:24:53 -0700

    v3d: Update an XXX comment for a path we handled in HW on V3D 4.x.

--- src/broadcom/compiler/vir.c
+++ src/broadcom/compiler/vir.c
@@ -572,7 +572,7 @@ v3d_lower_nir(struct v3d_compile *c)
 {
         struct nir_lower_tex_options tex_options = {
                 .lower_txd = true,
-                .lower_rect = false, /* XXX */
+                .lower_rect = false, /* XXX: Use this on V3D 3.x */
                 .lower_txp = ~0,
                 /* Apply swizzles to all samplers. */
                 .swizzle_result = ~0,
