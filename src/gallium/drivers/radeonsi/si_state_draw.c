066aa44fc56d277b3932d124c4962106c6962863
Marek Olšák <marek.olsak@amd.com>
2018-09-21 03:37:16 -0400

    radeonsi: fix a typo at CS_PARTIAL_FLUSH
    harmless

--- src/gallium/drivers/radeonsi/si_state_draw.c
+++ src/gallium/drivers/radeonsi/si_state_draw.c
@@ -962,7 +962,7 @@ void si_emit_cache_flush(struct si_context *sctx)
 	if (flags & SI_CONTEXT_CS_PARTIAL_FLUSH &&
 	    sctx->compute_is_busy) {
 		radeon_emit(cs, PKT3(PKT3_EVENT_WRITE, 0, 0));
-		radeon_emit(cs, EVENT_TYPE(V_028A90_CS_PARTIAL_FLUSH | EVENT_INDEX(4)));
+		radeon_emit(cs, EVENT_TYPE(V_028A90_CS_PARTIAL_FLUSH) | EVENT_INDEX(4));
 		sctx->num_cs_flushes++;
 		sctx->compute_is_busy = false;
 	}
